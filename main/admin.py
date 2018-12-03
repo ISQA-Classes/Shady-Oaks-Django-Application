f369bad0a8b0ff4e04eec04b22baf21735665b34
Cj <ckozeny@unomaha.edu>
2018-12-02 18:36:45 -0600

    Scheduling Feature added.

---
+++ main/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+from .models import Event
+from .models import Teetime
+
+
+class EventList(admin.ModelAdmin):
+    list_display = ('event_name', 'organization', 'contact_number' )
+    list_filter = 'organization'
+    search_fields = 'organization'
+    ordering = ['event_name']
+
+
+admin.site.register(Event)
+
+
+class TeeTimeList(admin.ModelAdmin):
+    list_display = ('Teetime_Name', 'Teetime_Start', 'Golfers_Allowed')
+    list_filter = 'Teetime_start'
+    search_fields = 'Teetime_Start'
+    ordering = ['Teetime_Start']
+
+
+admin.site.register(Teetime)
